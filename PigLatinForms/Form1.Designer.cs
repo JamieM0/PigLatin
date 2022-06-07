ce1c71e5f9afedef436ca53aa1948012c4eb1296
Jamie Matthews <jamiematthews48@hotmail.co.uk>
2022-06-07 14:20:59 +0100

    Add project files.

---
+++ PigLatinForms/Form1.Designer.cs
@@ -0,0 +1,126 @@
+﻿namespace PigLatinForms
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbInstructions1 = new System.Windows.Forms.Label();
+            this.txtCleartext = new System.Windows.Forms.TextBox();
+            this.btnConvert = new System.Windows.Forms.Button();
+            this.btnCopy = new System.Windows.Forms.Button();
+            this.txtConverted = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lbInstructions1
+            // 
+            this.lbInstructions1.AutoSize = true;
+            this.lbInstructions1.Font = new System.Drawing.Font("Segoe UI", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbInstructions1.Location = new System.Drawing.Point(113, 123);
+            this.lbInstructions1.Name = "lbInstructions1";
+            this.lbInstructions1.Size = new System.Drawing.Size(238, 37);
+            this.lbInstructions1.TabIndex = 0;
+            this.lbInstructions1.Text = "Input normal word";
+            // 
+            // txtCleartext
+            // 
+            this.txtCleartext.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtCleartext.Location = new System.Drawing.Point(56, 184);
+            this.txtCleartext.Name = "txtCleartext";
+            this.txtCleartext.Size = new System.Drawing.Size(347, 35);
+            this.txtCleartext.TabIndex = 1;
+            // 
+            // btnConvert
+            // 
+            this.btnConvert.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnConvert.Location = new System.Drawing.Point(120, 287);
+            this.btnConvert.Name = "btnConvert";
+            this.btnConvert.Size = new System.Drawing.Size(199, 47);
+            this.btnConvert.TabIndex = 2;
+            this.btnConvert.Text = "Convert to Pig Latin";
+            this.btnConvert.UseVisualStyleBackColor = true;
+            this.btnConvert.Click += new System.EventHandler(this.btnConvert_Click);
+            // 
+            // btnCopy
+            // 
+            this.btnCopy.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCopy.Location = new System.Drawing.Point(670, 287);
+            this.btnCopy.Name = "btnCopy";
+            this.btnCopy.Size = new System.Drawing.Size(92, 47);
+            this.btnCopy.TabIndex = 5;
+            this.btnCopy.Text = "Copy";
+            this.btnCopy.UseVisualStyleBackColor = true;
+            this.btnCopy.Click += new System.EventHandler(this.btnCopy_Click);
+            // 
+            // txtConverted
+            // 
+            this.txtConverted.Enabled = false;
+            this.txtConverted.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtConverted.Location = new System.Drawing.Point(540, 184);
+            this.txtConverted.Name = "txtConverted";
+            this.txtConverted.Size = new System.Drawing.Size(347, 35);
+            this.txtConverted.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(545, 123);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(342, 37);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Here\'s the word in Pig Latin";
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(967, 495);
+            this.Controls.Add(this.btnCopy);
+            this.Controls.Add(this.txtConverted);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnConvert);
+            this.Controls.Add(this.txtCleartext);
+            this.Controls.Add(this.lbInstructions1);
+            this.Name = "frmMain";
+            this.Text = "Pig Latin";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lbInstructions1;
+        private System.Windows.Forms.TextBox txtCleartext;
+        private System.Windows.Forms.Button btnConvert;
+        private System.Windows.Forms.Button btnCopy;
+        private System.Windows.Forms.TextBox txtConverted;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ PigLatinForms/Form1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PigLatinForms
+{
+    public partial class frmMain : Form
+    {
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCopy_Click(object sender, EventArgs e)
+        {
+            Clipboard.SetText(txtConverted.Text);
+        }
+
+        private void btnConvert_Click(object sender, EventArgs e)
+        {
+            string[] words = txtCleartext.Text.Split(' ');
+            foreach(string element in words)
+            {
+                convertToPigLatin(txtCleartext.Text);
+            }
+        }
+        
+        private void convertToPigLatin(string text)
+        {
+            string converted;
+            if (text[0] == 'a' || text[0] == 'e' || text[0] == 'i' || text[0] == 'o' || text[0] == 'u')
+            {
+                txtConverted.Text += text + "way" + " ";
+            }
+            else if (text[1] != 'a' && text[1] != 'e' && text[1] != 'i' && text[1] != 'o' && text[1] != 'u')
+            {
+                txtConverted.Text += text.Substring(2) + text[0] + text[1] + "ay" + " ";
+            }
+            else
+            {
+                txtConverted.Text += text.Substring(1) + text[0] + "ay" + " ";
+            }
+        }
+    }
+}
